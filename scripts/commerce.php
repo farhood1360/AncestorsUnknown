2295fab78b5776f2bc3464da9faa389e64878aeb
Farhood Rashidi <farhud.rashidi@gmail.com>
2014-07-30 00:00:50 -0500

    Update commerce.php

--- scripts/commerce.php
+++ scripts/commerce.php
@@ -1,29 +1,29 @@
 <?php # Script 1.1- commerce.php
 
-	/* Name: Commerce Web Portal/ Check Out Page
-	*  Author: Farhood Rashidi
-    *  Date: 07/22/2014
-    *  This page defines and gets the user's first name, last name, credit card and expiration date and then displays the confirmation message or error message. 
-	*/
+/* Name: Commerce Web Portal/ Check Out Page
+*  Author: Farhood Rashidi
+*  Date: 07/22/2014
+*  This page defines and gets the user's first name, last name, credit card and expiration date and then displays the confirmation message or error message. 
+*/
 	
-	# ************************** #
-	# ********** CLASSES ******* #
+# ************************** #
+# ********** CLASSES ******* #
 	
-	/* Class Checkout
-	*  The class contains five attributes:
-    *    - private $first_name
-	*	 - private $Last_name
-	*	 - private $credit
-	*	 - private $expiration
-	*	 - private $photo
-	*	The class contains two methods:
-	*	- __Constructor
-	*   - set_form()
-    *  
-	*/
+/* Class Checkout
+*  The class contains five attributes:
+*    - private $first_name
+*	 - private $Last_name
+*	 - private $credit
+*	 - private $expiration
+*	 - private $photo
+*	The class contains two methods:
+*	- __Constructor
+*   - set_form()
+*  
+*/
 	
-	class Checkout
-	{
+class Checkout
+{
 		// Declare the attributes:
 		private $first_name = "";
 		private $last_name = "";
@@ -100,45 +100,18 @@
 			}
 			return ($message);
 		}
-	} // End of Checkout class.
+} // End of Checkout class.
 	
-	# ********** END OF CLASSES ******* #
-	# ********************************* #
+# ********** END OF CLASSES ******* #
+# ********************************* #
 	
-	// Create a checkout form:
-	$customer = new Checkout();
-	$customer->first_name = $_POST['first_name'];
-	$customer->last_name = $_POST['last_name'];
-	$customer->credit = $_POST['credit'];
-	$customer->expiration = $_POST['expiration'];
-	$customer->photo = $_POST['photo'];
+// Create a checkout form:
+$customer = new Checkout();
+$customer->first_name = $_POST['first_name'];
+$customer->last_name = $_POST['last_name'];
+$customer->credit = $_POST['credit'];
+$customer->expiration = $_POST['expiration'];
+$customer->photo = $_POST['photo'];
 	
-	// Print the message:
-	$message = $customer->set_form();
-	
-?>
-
-<!doctype html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Check Out</title>
-    </head>
-    
-    <body>
-		<h1>Check Out Form</h1>
-        <h2>Enter your personal information to start check out processing.</h2>
-		<form action="commerce.php" method="post" enctype="multipart/form-data">
-			<p>First Name : <input name="first_name" type="text" size="10" maxlength="20" required autofocus/><br /></p>
-            <p>Last Name : <input name="last_name" type="text" size="10" maxlength="20" required/><br /></p>
-			<p>Credit Card Number : <input name="credit" type="text" size="10" maxlength="15" required/><br /></p>
-			<p>Exprition Date : <input name="expiration" type="text" size="4" maxlength="5" required/><br /></p>
-            <p>Personal Photo : <input name="photo" type="file" size="4" required/><br /></p>
-            <p>
-                <input name="info" type="submit" value="Submit" />
-                <input name="reset" type="reset" value="Reset" /><br />
-            </p>
-            <p><?php echo $message; ?></p>
-		</form>
-    </body>
-</html>
+// Print the message:
+$message = $customer->set_form();
