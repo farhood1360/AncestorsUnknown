b1e82666d55e0e5c351d20f1f69c4a35addac24c
Farhood Rashidi <farhud.rashidi@gmail.com>
2014-07-29 23:39:13 -0500

    Update and rename registration.php to register.php

--- register.php
+++ register.php
@@ -1,10 +1,12 @@
 <?php
+
 include_once 'include/functions.php';
 $user = new User();
+
 // Checking for user logged in or not
 if ($user->get_session())
 {
-	header("location:home.php");
+	header("home.php");
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") 
@@ -21,16 +23,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 		echo 'Registration failed. Email or Username already exits please try again';
 	}
 }
-?>
-//HTML Code
-<form method="POST" action="register.php" name='reg' >
-Full Name
-<input type="text" name="name"/>
-Username
-<input type="text" name="username"/>
-Password
-<input type="password" name="password"/>
-Email
-<input type="text" name="email"/>
-<input type="submit" value="Register"/>
-</form>
